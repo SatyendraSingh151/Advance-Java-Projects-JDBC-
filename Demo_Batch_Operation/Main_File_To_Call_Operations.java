42e137855330e0b9567a4b5b8ddff27d9dab75d6
Satyendra Singh <151satyendrasingh@gmail.com>
2025-09-24 11:01:09 +0530

    Add files via upload

---
+++ Demo_Batch_Operation/Main_File_To_Call_Operations.java
@@ -0,0 +1,44 @@
+package Demo_Batch_Operation;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Main_File_To_Call_Operations {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//		making the object reference of the class which has all the operations defined in it
+		Operations_Performed ref = new Operations_Performed();
+		
+		Scanner sc = new Scanner(System.in);
+
+		boolean status = true;
+		while(status) {
+			System.out.println("1 - Operations\n2 - View records\n3 - Exit");
+			System.out.print("\nEnter your choice to perform the action : ");
+			int choice = sc.nextInt();
+			System.out.println();
+			
+			switch(choice) {
+				case 1:{
+					ref.tasks();
+					break;
+				}
+				case 2:{
+					ref.view();
+					break;
+				}
+				case 3:{
+					status = false;
+					System.out.println("Thank you for using this Employee Management System.");
+					break;
+				}
+				default :{
+					System.out.println("Opps!, you made a wrong choice, Please try again.");
+				}
+			}
+			
+		}
+
+	}
+
+}
